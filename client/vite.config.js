e40269f8477943e77b75651f1913c6913483cbb8
phuocnnt464 <96932969+phuocnnt464@users.noreply.github.com>
2026-03-04 11:34:00 +0700

    update optional position center left right watermark

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
-      // Proxy /assets để client lấy watermark mặc định từ server
       '/assets': {
         target: 'http://localhost:3000',
         changeOrigin: true,
