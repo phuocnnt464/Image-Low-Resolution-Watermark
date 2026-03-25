15546a0e4c9f8cb5acb5077ddd0af8300cd690ad
phuocnnt464 <96932969+phuocnnt464@users.noreply.github.com>
2026-03-25 11:03:44 +0700

    fixx

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express    = require('express');
 const cors       = require('cors');
 const path       = require('path');
+const fs         = require('fs');
 const bodyParser = require('body-parser');
 const imageRoutes = require('./src/routes/imageRoutes');
 const videoRoutes = require('./src/routes/videoRoutes');
